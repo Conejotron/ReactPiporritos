15bf31ba3cd707a6a0c19aef74f3baf3cd217897
Conejotron <daniel.arenasc@gmail.com>
2022-05-28 23:01:44 -0400

    segundo desafio

---
+++ src/componentes/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Indice',
+        url: '#',
+        cName: 'nav-links'     
+    },
+    {
+        title: 'Servicios',
+        url: '#',
+        cName: 'nav-links'     
+    },
+    {
+        title: 'Productos',
+        url: '#',
+        cName: 'nav-links'     
+    },
+    {
+        title: 'Contactanos',
+        url: '#',
+        cName: 'nav-links'     
+    },
+    {
+        title: 'Inscribete',
+        url: '#',
+        cName: 'nav-links'     
+    },
+]
